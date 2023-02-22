418460d8ebf0464273edc142e28563b1016a95c4
차경훈 <cksuwjr@naver.com>
2023-02-22 17:12:58 +0900

    점프 공격 추가

--- Assets/CAM.cs
+++ Assets/CAM.cs
@@ -8,6 +8,7 @@ public class CAM : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Target == null) Target = GameObject.FindGameObjectWithTag("Player");
         if (Target == null) return;
 
         transform.position = new Vector3(Target.transform.position.x, transform.position.y, transform.position.z);
